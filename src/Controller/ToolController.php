3f38b8126b0ed1022e7b04c9299dc97df94ee959
ValentinGallo <valentingallo.pro@gmail.com>
2020-05-14 17:46:15 +0200

    Add 'Create tool' & 'list_tool'

--- src/Controller/ToolController.php
+++ src/Controller/ToolController.php
@@ -7,24 +7,27 @@ use App\Form\ToolType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ToolController extends AbstractController
 {
     /**
-     * @Route("/tool", name="tool")
+     * @Route("/annonces", name="list_tool")
      */
-    public function index()
+    public function listTool(EntityManagerInterface $manager)
     {
-        return $this->render('tool/index.html.twig', [
-            'controller_name' => 'ToolController',
+        $tools = $manager->getRepository(Tool::class)->findAll();
+        return $this->render('tool/list.html.twig', [
+            'list_tools' => $tools,
         ]);
     }
 
     /**
      * @Route("/creation_annonce", name="create_tool")
      */
-    public function createTool(Request $request, EntityManagerInterface $manager)
+    public function createTool(Request $request, EntityManagerInterface $manager, SluggerInterface $slugger)
     {
         $tool = new Tool();
 
@@ -33,14 +36,36 @@ class ToolController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
-            $tool->setDate(new \DateTime('@'.strtotime('now')));
+            /** @var UploadedFile $pictureFile */
+            $pictureFile = $form->get('picture')->getData();
+            if ($pictureFile) {
+                $originalFilename = pathinfo($pictureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$pictureFile->guessExtension();
 
+                // Move the file to the directory where brochures are stored
+                try {
+                    $pictureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $tool->setPicture($newFilename);
+            }
+            $tool->setDate(new \DateTime('@'.strtotime('now')));
+            $tool->setCreator($this->getUser());
             $manager->persist($tool);
             $manager->flush();
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('list_tool');
         }
 
-        return $this->render('tool/index.html.twig', ['form' => $form->createView()]);
+        return $this->render('tool/createTool.html.twig', ['form' => $form->createView()]);
     }
 }
--- src/Form/ToolType.php
+++ src/Form/ToolType.php
@@ -8,8 +8,10 @@ use App\Entity\Department;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ToolType extends AbstractType
@@ -22,10 +24,28 @@ class ToolType extends AbstractType
                 'attr' => ['rows'=> '4'],
             ])
             ->add('picture', FileType::class, [
-                'attr' => ['accept'=> 'image/png, image/jpeg']
+                'attr' => ['accept' =>'image/png, image/jpeg'],
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Le site accepte seulement les images aux format PNG ou JPEG',
+                    ])
+                ],
             ])
             ->add('prices')
             ->add('address')
+            ->add('type', ChoiceType::class, [
+                'choices'  => [
+                    'Offre' => 'Offre',
+                    'Demande' => true,
+                ],
+            ])
             ->add('department', EntityType::class, [
                 'class' => Department::class,
                 'required' => true,
